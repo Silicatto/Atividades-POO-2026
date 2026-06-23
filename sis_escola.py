61b1baf25d092e37025404794a2ef77bb77d7dd9
Sillicatto <ggzf448@gmail.com>
2026-06-23 14:43:28 -0300

    Add files via upload

---
+++ sis_escola.py
@@ -0,0 +1,33 @@
+from datetime import date 
+
+class pessoa:
+    def __init__(self, CPF:str, nome:str) -> None:
+        self.CPF = CPF
+        self.nome = nome
+        self.listaPresenca = []
+    def addPresenca(self, ID:int, data:date) -> None:
+        self.listaPresenca.append(Presenca(ID, data))
+
+class docente(pessoa):
+    def __init__(self, CPF:str, nome:str, salario:float) -> None:
+        super().__init__(CPF, nome)
+        self.salario = salario
+
+class estudante(pessoa):
+    def __init__(self, CPF:str, nome:str, RA:str) -> None:
+        super().__init__(CPF, nome)
+        self.RA = RA
+        self.listaAtividade = []
+    def addAtividade(self, atv:Atividade) -> None:
+        self.listaAtividade.append(atv)
+    
+class Presenca:
+    def __init__(self, ID:int, data:date) -> None:
+        self.ID = ID
+        self.data = data
+
+class Atividade:
+    def __init__(self, codigo:int, descricao:str, valor:float) -> None:
+        self.codigo = codigo
+        self.descricao = descricao
+        self.valor = valor
