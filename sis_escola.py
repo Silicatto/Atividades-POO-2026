e1d555ba59597e6ddbf6b9f75da717eea12a694f
Sillicatto <ggzf448@gmail.com>
2026-06-23 14:53:36 -0300

    Reorganize class definitions in sis_escola.py

--- sis_escola.py
+++ sis_escola.py
@@ -1,5 +1,10 @@
 from datetime import date 
 
+class Presenca:
+    def __init__(self, ID:int, data:date) -> None:
+        self.ID = ID
+        self.data = data
+
 class pessoa:
     def __init__(self, CPF:str, nome:str) -> None:
         self.CPF = CPF
@@ -13,6 +18,12 @@ class docente(pessoa):
         super().__init__(CPF, nome)
         self.salario = salario
 
+class Atividade:
+    def __init__(self, codigo:int, descricao:str, valor:float) -> None:
+        self.codigo = codigo
+        self.descricao = descricao
+        self.valor = valor
+
 class estudante(pessoa):
     def __init__(self, CPF:str, nome:str, RA:str) -> None:
         super().__init__(CPF, nome)
@@ -21,13 +32,3 @@ class estudante(pessoa):
     def addAtividade(self, atv:Atividade) -> None:
         self.listaAtividade.append(atv)
     
-class Presenca:
-    def __init__(self, ID:int, data:date) -> None:
-        self.ID = ID
-        self.data = data
-
-class Atividade:
-    def __init__(self, codigo:int, descricao:str, valor:float) -> None:
-        self.codigo = codigo
-        self.descricao = descricao
-        self.valor = valor
